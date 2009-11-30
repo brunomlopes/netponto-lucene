3a423b402000b8636dcf761467ecbe9ec929a273
brunomlopes <brunomlopes@gmail.com>
2009-11-30 01:47:50 +0000

    Use a project to store common files

---
+++ source/Common/Configuration.cs
@@ -0,0 +1,14 @@
+﻿namespace Common
+{
+    public class Configuration
+    {
+        public string FilesDirectory { get; private set; }
+        public string IndexDirectory { get; protected set; }
+
+        public Configuration()
+        {
+            FilesDirectory = @"F:\ptwiki-latest-pages-articles.xml\files";
+            IndexDirectory = @"F:\ptwiki-latest-pages-articles.xml\index";
+        }
+    }
+}
