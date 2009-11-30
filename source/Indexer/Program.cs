8ea59d344c7ac4c155949c48f2608148705b885c
brunomlopes <brunomlopes@gmail.com>
2009-11-30 02:16:13 +0000

    Portuguese stopwords

--- source/Indexer/Program.cs
+++ source/Indexer/Program.cs
@@ -8,10 +8,33 @@ namespace Indexer
 {
     class Program
     {
+        public static string[] PortugueseStopWords =
+            {
+                "a", "à", "ainda", "alem", "ambas", "ambos", "antes",
+                "ao", "aonde", "aos", "apos", "aquele", "aqueles",
+                "as", "assim", "com", "como", "contra", "contudo",
+                "cuja", "cujas", "cujo", "cujos", "da", "das", "de",
+                "dela", "dele", "deles", "demais", "depois", "desde",
+                "desta", "deste", "dispoe", "dispoem", "diversa",
+                "diversas", "diversos", "do", "dos", "durante", "e", "é", 
+                "ela", "elas", "ele", "eles", "em", "entao", "entre",
+                "essa", "essas", "esse", "esses", "esta", "estas",
+                "este", "estes", "ha", "isso", "isto", "logo", "mais",
+                "mas", "mediante", "menos", "mesma", "mesmas", "mesmo",
+                "mesmos", "na", "nas", "nao", "não", "nas", "nem", "nesse", "neste",
+                "nos", "no", "o", "os", "ou", "outra", "outras", "outro", "outros",
+                "pela", "pelas", "pelo", "pelos", "perante", "pois", "por",
+                "porque", "portanto", "proprio", "propios", "quais", "qual",
+                "qualquer", "quando", "quanto", "que", "quem", "quer", "se",
+                "seja", "sem", "sendo", "seu", "seus", "sob", "sobre", "sua",
+                "suas", "tal", "tambem", "teu", "teus", "toda", "todas", "todo",
+                "todos", "tua", "tuas", "tudo", "um", "uma", "umas", "uns"
+            };
+
         static void Main(string[] args)
         {
             var indexWriter = new IndexWriter(Configuration.IndexDirectory,
-                                              new StandardAnalyzer(),
+                                              new StandardAnalyzer(PortugueseStopWords),
                                               true,
                                               IndexWriter.MaxFieldLength.UNLIMITED);
 
